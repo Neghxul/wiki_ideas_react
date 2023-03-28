0017328f88e73c77f39620629bed722a767f1892
Neghxul <cgrr29us@gmail.com>
2023-03-28 13:49:30 -0600

    commit

--- api/index.js
+++ api/index.js
@@ -8,14 +8,6 @@ import multer from "multer";
 
 const app = express();
 
-/*app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });*/
-
-
 app.use(express.json());
 app.use(cookieParser());
 
